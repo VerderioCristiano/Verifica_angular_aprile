13074b6c4dcc3f1bac5bfa7b31f2960f3ec4f670
Cristiano Verderio <verderio.cristiano@iisgalvanimi.edu.it>
2024-04-08 08:02:45 +0000

    fine

--- Verifica/src/app/list/list.component.ts
+++ Verifica/src/app/list/list.component.ts
@@ -10,10 +10,10 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
   prodotti = [
-    {nome:'fragola', immagine:'./assets/fragole.jpg', categoria:'alimentari', bool: false, costo:'10$',quantità:'restanti: 30', marca:'solare',},
-    {nome:'mela', immagine:'./assets/mela.jpg', categoria:'alimentari', bool: false, costo:'5$',quantità:'restanti: 20',marca:'melinda',},
-    {nome:'passeggino', immagine:'./assets/passeggino.jpg', categoria:'nonAlimentari', bool: false, costo:'50$', quantità:'restanti: 15',marca:'chicco',},
-    {nome:'ruota', immagine:'./assets/ruota.jpg', categoria:'nonAlimentari', bool: false, costo:'20$',quantità:'restanti: 10',marca:'Michelin',},
+    {nome:'fragola', immagine:'./assets/fragole.jpg', categoria:'alimentari', bool: false, costo:'10$',quantita:'restanti: 30', marca:'solare',desc:'una classica fragola in grado di sprigionare un sapore in bocca fantastico, gustatene quante ne volete, ma attenti a non mangiarne troppe altrimenti..'},
+    {nome:'mela', immagine:'./assets/mela.jpg', categoria:'alimentari', bool: false, costo:'5$',quantita:'restanti: 20',marca:'melinda',desc:'la mela è il più comune tra i frutti da tavolo, e queste mele sono le più gustose in città. '},
+    {nome:'passeggino', immagine:'./assets/passeggino.jpg', categoria:'nonAlimentari', bool: false, costo:'50$', quantita:'restanti: 15',marca:'chicco',desc:'Il passeggino è una specie di seggiolino con ruote per portare a passeggio i bambini piccoli sino a circa 36 mesi di età. In commercio ne esistono diversi tipi: a tre o a quattro ruote, leggeri, superleggeri, gemellari, trio in combinazione di navicella e seggiolino auto. '},
+    {nome:'ruota', immagine:'./assets/ruota.jpg', categoria:'nonAlimentari', bool: false, costo:'20$',quantita:'restanti: 10',marca:'Michelin', desc:'La ruota è un organo meccanico a forma circolare che, attraverso la rotazione attorno ad un asse, consente di sostituire il moto di strisciamento con il moto di rotolamento. ',},
     ]
   
   click(prodotti: {nome: string}){
